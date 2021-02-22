3b5440e2b37c50c5e4cab19911d7d7e90b77df3d
James Goode <james_goode@hotmail.co.uk>
2021-02-22 11:49:38 +0000

    Add project files.

---
+++ EncounterGenerator/Controllers/EncounterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncounterGenerator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EncounterController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to the encounter generator.";
+        }
+    }
+}
